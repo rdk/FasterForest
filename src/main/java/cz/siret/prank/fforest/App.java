55b887823136575a3afce5aa2dcea82d07b42f34
rdk <rdk@users.noreply.github.com>
2017-10-18 15:21:21 +0200

    initial commit. code moved from P2Rank project

---
+++ src/main/java/cz/siret/prank/fforest/App.java
@@ -0,0 +1,13 @@
+package cz.siret.rdk.fforest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

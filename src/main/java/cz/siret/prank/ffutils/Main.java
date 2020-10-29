38c6e4ecd1ab715951b67f73db6488045ff88c5a
rdk <rdk@users.noreply.github.com>
2020-10-29 23:24:30 +0100

    add main() for Graal testing

---
+++ src/main/java/cz/siret/prank/ffutils/Main.java
@@ -0,0 +1,12 @@
+package cz.siret.prank.ffutils;
+
+/**
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("FasterForset library test.");
+    }
+    
+}

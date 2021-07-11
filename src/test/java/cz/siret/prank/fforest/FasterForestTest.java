2aea4a5cd711e5fe5afbf720b95b01788ccc528b
rdk <rdk@users.noreply.github.com>
2021-07-11 15:31:09 +0200

    add dummy tests

---
+++ src/test/java/cz/siret/prank/fforest/FasterForestTest.java
@@ -0,0 +1,18 @@
+package cz.siret.prank.fforest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class FasterForestTest {
+
+    @Test
+    public void testCreate() {
+        FasterForest ff = new FasterForest();
+    }
+
+
+}
---
+++ src/test/java/cz/siret/prank/fforest2/FasterForest2Test.java
@@ -0,0 +1,17 @@
+package cz.siret.prank.fforest2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class FasterForest2Test {
+
+    @Test
+    public void testCreate() {
+        FasterForest2 ff = new FasterForest2();
+    }
+
+}
